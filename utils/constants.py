543af3a7b5f5bad54a3adad4f924b6607a542203
nandrasana <miora.rabearivelo@pulse.mg>
2025-08-01 11:53:39 +0300

    add api config to constant file

--- utils/constants.py
+++ utils/constants.py
@@ -12,7 +12,13 @@ logger = setup_logging()
 conf = configparser.ConfigParser()
 conf.read(os.path.join(os.path.dirname(__file__), "../config/config.conf"))
 
+# AWS configuration
 AWS_ACCESS_KEY = conf.get("aws", "aws_access_key")
 AWS_SECRET_ACCESS_KEY = conf.get("aws", "aws_secret_key")
 AWS_REGION = conf.get("aws", "aws_region")
 AWS_BUCKET_NAME = conf.get("aws", "aws_bucket_name")
+
+# API configuration
+API_KEY = conf.get("api", "api_key")
+API_URL = conf.get("api", "api_url")
+API_REGIONS = conf.get("api", "api_regions").split(", ")
