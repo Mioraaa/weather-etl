a5519da28498eabf5ac98dc37baa4df96adfcd41
nandrasana <miora.rabearivelo@pulse.mg>
2025-08-01 16:38:49 +0300

    update airflow conf

---
+++ dags/wearher_dag.py
@@ -0,0 +1,53 @@
+
+
+import os, sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from airflow import DAG # type:ignore
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from datetime import datetime
+from pipelines.weather_pipeline import WeatherPipeline
+
+default_args = {
+    "owner": "miora",
+    "start_date": datetime(2025, 1, 7),
+}
+
+file_postfix = datetime.now().strftime("%Y%m%d")
+
+
+
+@task(identifier="connect_to_s3")
+def connect_to_s3():
+    weather_pipeline = WeatherPipeline()
+    return weather_pipeline.run_connect_s3()
+
+@task(identifier="create_bucket")
+def create_bucket():
+    weather_pipeline = WeatherPipeline()
+    return weather_pipeline.run_create_bucket()
+
+@task(identifier="fetch_raw_data_region_weather")
+def fetch_raw_data_region_weather():
+    weather_pipeline = WeatherPipeline()
+    return weather_pipeline.run_fetch_raw_data_region_weather()
+
+@task(identifier="upload_data_into_bucket")
+def upload_data_into_bucket():
+    weather_pipeline = WeatherPipeline()
+    return weather_pipeline.run_upload_data_into_bucket()
+
+with DAG(
+    dag_id="weather_dag",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    connect_to_s3_task = connect_to_s3()
+    create_bucket_task = create_bucket()
+    fetch_raw_data_region_weather_task = fetch_raw_data_region_weather()
+    upload_data_into_bucket_task = upload_data_into_bucket()
+
+connect_to_s3_task >> create_bucket_task >> fetch_raw_data_region_weather_task >> upload_data_into_bucket_task
