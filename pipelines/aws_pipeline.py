03769ca2f1e25a17817ffe7d402e1902334da040
nandrasana <miora.rabearivelo@pulse.mg>
2025-08-14 10:44:25 +0300

    fix import

--- pipelines/aws_pipeline.py
+++ pipelines/aws_pipeline.py
@@ -6,7 +6,7 @@ import glob
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from tasks.weather_etl import BucketOperation
-from utils.constants import logger, AWS_BUCKET_NAME_INPUT, OUTPUT_DIR
+from utils.constants import logger, AWS_BUCKET_NAME_OUPUT, OUTPUT_DIR
 
 class AwsPipeline:
     
@@ -16,14 +16,14 @@ class AwsPipeline:
    
     def run_create_bucket(self):
         s3 = self.bucket_operation.connect_to_s3()
-        if not self.bucket_operation.create_bucket(s3, AWS_BUCKET_NAME_INPUT):
+        if not self.bucket_operation.create_bucket(s3, AWS_BUCKET_NAME_OUPUT):
             logger.error("Failed to create s3 bucket.")
             return False
         return True
     
     def run_load_data_into_bucket(self):
         s3 = self.bucket_operation.connect_to_s3()
-        if not self.bucket_operation.upload_data_into_bucket(OUTPUT_DIR, AWS_BUCKET_NAME_INPUT, s3):
+        if not self.bucket_operation.upload_data_into_bucket(OUTPUT_DIR, AWS_BUCKET_NAME_OUPUT, s3):
             logger.error("Failed to upload data into bucket.")
             return False
         return True
